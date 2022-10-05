687e813f28a8428ebfee3e7f03603092c29efe7f
tommyzin <106184145+tommyzin@users.noreply.github.com>
2022-10-05 11:56:19 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -16,3 +16,22 @@ function escreva() {
 
 
 }
+
+let lista = ["Terra", "Marte", "Saturno", "Jupiter", "Vênus", "Mercurio"];
+
+function mostraLista(){
+    document.write("Tamanho da lista: " + lista.length + "<br>")
+    
+    for(let i = 0; i < lista.length; i++){ 
+        document.write("Planeta " + lista[i] + "<br>")
+    }
+}
+
+function repete(){
+    for(let i = 1; i <= 10; i++){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <=10; j++){
+            document.write(i+ " x " + j + " = " +(i*j) + "<br>");
+        }
+    }
+}
