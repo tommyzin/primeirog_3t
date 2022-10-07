33fe2b80b511a5ef0705092f38596a99aee7275d
tommyzin <106184145+tommyzin@users.noreply.github.com>
2022-10-07 12:48:12 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -5,7 +5,7 @@ function total(){
     let m = 0;
     for(let i = 1; i <= t; i++){
      m = c * (1 + (j/100));
-    document.write("Valor no mês  " +i " = " + m + "<br>");
+    document.write("Valor no mês  " + i " = " + m + "<br>");
      c = m;
     } 
     document.write("Montante: " + m);
